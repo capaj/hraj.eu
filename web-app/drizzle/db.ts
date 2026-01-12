7b1cfbeb793882d4e4ac15745a57493f6bd2198e
Jiri Spac <capajj@gmail.com>
2026-01-12 16:01:44 +0100

    add weird fetchCompat fetch helper

--- web-app/drizzle/db.ts
+++ web-app/drizzle/db.ts
@@ -3,10 +3,29 @@ import { createClient } from '@libsql/client'
 import * as schema from './schema'
 import { env } from 'cloudflare:workers'
 
+const fetchCompat: typeof fetch = async (input, init) => {
+  if (typeof input === 'string' || input instanceof URL) {
+    return fetch(input, init)
+  }
+
+  if (input && typeof input === 'object' && 'url' in input) {
+    const request = input as Request
+
+    return fetch(request.url, {
+      method: request.method,
+      headers: request.headers,
+      body: request.body ?? undefined,
+      signal: request.signal ?? undefined
+    })
+  }
+
+  return fetch(input as Request, init)
+}
 
 const turso = createClient({
   url: env.TURSO_DATABASE_URL!,
-  authToken: env.TURSO_AUTH_TOKEN!
+  authToken: env.TURSO_AUTH_TOKEN!,
+  fetch: fetchCompat // why is this necessary???? TODO figure out
 })
 
 export const db = drizzle(turso, { schema })
